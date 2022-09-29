65b3c7ced8ad38991d7e94c4b788d40484825c1d
Greg Werbin <gwerbin@users.noreply.github.com>
2022-09-29 18:18:50 -0400

    Use fancier types & change what repr does
    I have no idea if this works, editing via Github

--- lazy_string.py
+++ lazy_string.py
@@ -3,10 +3,12 @@ import sys
 if sys.version_info >= (3, 9):
   from collections.abc import Callable
   from collections.abc import Mapping
-
   List  = list
   Tuple = tuple
-
+  if sys.version_info >= (3, 10):
+    from typing import ParamSpec
+  else:
+    from typing_extensions import ParamSpec
 else:
   from typing import Callable
   from typing import Mapping
@@ -15,49 +17,65 @@ else:
 
 from collections import UserString
 
-from typing import Any
-from typing import TypeVar
-from typing import Union
+from typing import Any, TypeVar, Union, overload
 
 
-LazyString = TypeVar("LazyString")
+_P = ParamSpec('_P')
+_Self = TypeVar("_Self", bound='LazyString')
 
 
 class LazyString(UserString):
   """
   A string with delayed evaluation.
 
-  :param func:   Callable (e.g., function) returning a string.
-  :param args:   Optional positional arguments which will be passed to the ``func``.
+  :param func: Callable (e.g., function) returning a string.
+  :param args: Optional positional arguments which will be passed to the ``func``.
   :param kwargs: Optional keyword arguments which will be passed to the ``func``.
-
   """
 
-  __slots__ = ("_func", "_args", )
+  __slots__ = ("_func", "_args", "_kwargs")
+  
+  _func: Callable[_P, str]
+  _args: _P.args
+  _kwargs: _P.kwargs
 
-  def __new__(cls, func: Union[Callable, str], *args: Tuple, **kwargs: Mapping) -> object:
+  @overload
+  def __new__(cls, func: Callable[_P, str], *args: _P.args, **kwargs: _P.kwargs) -> UserString: ...
+  @overload
+  def __new__(cls, func: str) -> str: ...
+  def __new__(cls, func: Union[_P, str], str], *args: _P.args, **kwargs: _P.kwargs) -> Union[UserString, str]:
     if isinstance(func, str):
-      # Many UserString's functions like `lower`, `__add__` and so on wrap
-      # returned values with a call to `self.__class__(...)` to ensure the
-      # result is of the same type as the original class.
-      # However, as the result of all of such methods is always a string,
-      # there's no need to create a new instance of a `LazyString`
+      # Many `UserString` functions like `lower` and `__add__` wrap
+      # returned values with a call to `self.__class__(...)` to ensure that
+      # the result is of the same type as the original class.
+      # However, the result of all of such methods is always a string,
+      # so there's no need to create a new instance of a `LazyString`.
       return func
+    else:
+      return UserString.__new__(cls)
 
-    return object.__new__(cls)
-
-  def __init__(self, func: Callable[..., str], *args: Tuple, **kwargs: Mapping) -> None:
-    self._func   = func
-    self._args   = args
+  def __init__(self, func: Callable[_P, str], *args: _P.args, **kwargs: _P.kwargs) -> None:
+    self._func = func
+    self._args = args
     self._kwargs = kwargs
+
+  # Custom methods
 
   @property
   def data(self) -> str:
     return self._func(*self._args, **self._kwargs)
 
+  def __copy__(self: _Self) -> _Self:
+    return LazyString(self._func, *self._args, **self._kwargs)
+
+  def __repr__(self) -> str:
+    return f"{self.__class__.__name__}({self._func!r}, *{self._args!r}, **{self._kwargs!r})"
+
   def __getnewargs_ex__(self) -> Tuple[Tuple, Mapping]:
     args = (self._func, ) + self._args
     return (args, self._kwargs)
+
+  # Proxy methods
 
   def __getstate__(self) -> Tuple[Callable, Tuple, Mapping]:
     return (self._func, self._args, self._kwargs)
@@ -65,18 +83,6 @@ class LazyString(UserString):
   def __setstate__(self, state: Tuple[Callable, Tuple, Mapping]) -> None:
     self._func, self._args, self._kwargs = state
 
+  # TODO: Is this needed? Why wouldn't it already be part of UserString?
   def __getattr__(self, name: str) -> Any:
     return getattr(self.data, name)
-
-  def __dir__(self) -> List[str]:
-    return dir(str)
-
-  def __copy__(self) -> LazyString:
-    return self
-
-  def __repr__(self) -> str:
-    try:
-      r = repr(str(self.data))
-      return f"{self.__class__.__name__}({r})"
-    except Exception:
-      return "<%s broken>" % self.__class__.__name__
